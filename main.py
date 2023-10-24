6f73143b086f5fe0b778279d51686f21c625be6f
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-10-24 19:18:13 -0300

    FEAT(main): Adicionando insert

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from type_excel import TypeExcel
 from type_pdf import TypePdf
 from type_xml import TypeXml
 
+from use_mysql import useMySql
+from use_odbc import useOdbc
+
 def get_type_data(type: str):
     typeFile = type.upper()
     if typeFile == ".JSON":
@@ -22,8 +25,12 @@ def init(path_file):
     _, file_extension = os.path.splitext(path_file)
     typeData = get_type_data(file_extension)
     data = typeData.get_data_by_file(path_file)
-    stringToInsert = typeData.insert_data_by_bd(data)
-    print(stringToInsert)
+    stringToInsertList = typeData.insert_data_by_bd(data)
+
+    db = useMySql()
+    for stringToInsert in stringToInsertList:
+        db.insert_table(stringToInsert)
+
 
 
 init("./files-examples/excel_registros.pdf")
